5698734bed4a4c8bcaa738101d2a85b55debd42e
caren1 <czarnocki.w@wp.pl>
2020-11-20 16:19:09 +0100

    outputting burger ingredients dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,11 +5,19 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 const Burger = (props) => {
 
 
+    const transformedIngredients = Object.keys(props.ingredients)
+    .map(ingredientKey => {
+        return [...Array(props.ingredients[ingredientKey])]
+        .map((_, i) => {
+            return <BurgerIngredient key={ingredientKey + 1} type={ingredientKey}/>
+        })
+    });
+    // transforms an object into the array of string of the keys
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="cheese"/>
-            <BurgerIngredient type="meat"/>
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom"/>
         </div>
     )
