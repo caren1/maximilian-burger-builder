9c7336603e362a594c95cbcc5020e79f661c4402
caren1 <czarnocki.w@wp.pl>
2020-11-23 20:05:11 +0100

    adding reusable navigation items

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,6 +2,7 @@ import React from 'react'
 import classes from './Toolbar.module.css'
 
 import Logo from '../../Logo/Logo'
+import NavigationItems from '../NavigationItems/NavigationItems'
 
 const Toolbar = (props) => {
     return (
@@ -9,7 +10,7 @@ const Toolbar = (props) => {
             <div className="">MENU</div>
             <Logo />
             <nav>
-
+                <NavigationItems />
             </nav>
         </header>
     )
