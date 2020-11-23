8f1ede47ee410747ddbf237710c804972a9026ce
caren1 <czarnocki.w@wp.pl>
2020-11-23 19:46:34 +0100

    adding a Toolbar component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Auxiliary from '../../hoc/Auxiliary'
+import Toolbar from '../Navigation/Toolbar/Toolbar'
 
 import classes from './Layout.module.css'
 
 const Layout = (props) => {
     return (
         <Auxiliary>
+        <Toolbar />
         <div>Toolbar, SideDrawer, Backdrop</div>
         <main className={classes.Content}>
             {props.children}
---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import classes from './Toolbar.module.css'
+
+const Toolbar = (props) => {
+    return (
+        <header className={classes.Toolbar}>
+            <div className="">MENU</div>
+            <div className="">LOGO</div>
+            <nav>
+
+            </nav>
+        </header>
+    )
+}
+
+export default Toolbar
