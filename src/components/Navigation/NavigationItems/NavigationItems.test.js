68c4c20dd2f41c8652559caea1229b7057882138
caren1 <czarnocki.w@wp.pl>
2020-12-17 15:41:08 +0100

    beforeach + setprops

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -1,32 +1,44 @@
 // *.test.js is important to run the tests
 
 // imported for the purpose of using as an argument to shallow function
-import React from 'react'
-import NavigationItems from './NavigationItems'
-import NavigationItem from './NavigationItem/NavigationItem'
+import React from "react";
+import NavigationItems from "./NavigationItems";
+import NavigationItem from "./NavigationItem/NavigationItem";
 
 // for the enzyme to work we have to first configure it (adapter + configure)
-import { configure, shallow } from 'enzyme'
+import { configure, shallow } from "enzyme";
 // shallow allows to render content for the component, but it is not loaded deeply
 
-import Adapter from 'enzyme-adapter-react-16'
+import Adapter from "enzyme-adapter-react-16";
 
 configure({
-    adapter: new Adapter()
-})
-
+  adapter: new Adapter(),
+});
 
 // takes 2 arguments, test description and actual test scenarios
-describe('Navigation Items', () => {
+describe("Navigation Items", () => {
+  let wrapper;
 
-    // also takes 2 arguments and this is individual test scenario (name + function)
-    it('should render 2 NavItem, if not authenticated', () => {
-        // we want to create an instance of component and then look into it and see if the condition was matched
-        // for render we'll use Enzyme that will create a standalone instance of the component, without the need to render whole application
-        const wrapper = shallow(<NavigationItems />);
+  // helper method that allows to do something before each test is run
+  beforeEach(() => {
+    wrapper = shallow(<NavigationItems />);
+  });
 
-        // then we use expect method, where we define what is the expected result
-        // not a JSX element, but regular function
-        expect(wrapper.find(NavigationItem)).toHaveLength(2);
-    })
-})
+  // also takes 2 arguments and this is individual test scenario (name + function)
+  it("should render 2 NavItem(s), if not authenticated", () => {
+    // we want to create an instance of component and then look into it and see if the condition was matched
+    // for render we'll use Enzyme that will create a standalone instance of the component, without the need to render whole application
+    // const wrapper = shallow(<NavigationItems />);
+
+    // then we use expect method, where we define what is the expected result
+    // not a JSX element, but regular function
+    expect(wrapper.find(NavigationItem)).toHaveLength(2);
+  });
+
+  it("should render 3 NavItem(s), if authenticated", () => {
+    // const wrapper = shallow(<NavigationItems isAuthenticated={true} />);
+    // wrapper = shallow(<NavigationItems isAuthenticated={true} />)
+    wrapper.setProps({ isAuthenticated: true });
+    expect(wrapper.find(NavigationItem)).toHaveLength(3);
+  });
+});
