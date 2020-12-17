5e364272d430cf655f37b25f43692d048d3a9d15
caren1 <czarnocki.w@wp.pl>
2020-12-17 13:37:33 +0100

    fixed localstorage for user authentication

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -59,16 +59,16 @@ export const authCheckState = () => {
       dispatch(logout());
     } else {
       const expirationDate = new Date(localStorage.getItem("expirationDate"));
-      if (expirationDate > new Date()) {
+      if (expirationDate <= new Date()) {
+        dispatch(logout());
+      } else {
         const userId = localStorage.getItem("userId");
         dispatch(authSuccess(token, userId));
         dispatch(
           checkAuthTimeout(
-            expirationDate.getSeconds() - new Date().getSeconds()
+            (expirationDate.getTime() - new Date().getTime()) / 1000
           )
         );
-      } else {
-        dispatch(logout());
       }
     }
   };
@@ -95,7 +95,7 @@ export const auth = (email, password, isSignUp) => {
       .then((response) => {
         // console.log(response);
         const expirationDate = new Date(
-          new Date().getTime() + response.data.expiresIn / 1000
+          new Date().getTime() + response.data.expiresIn * 1000
         );
         localStorage.setItem("token", response.data.idToken);
         localStorage.setItem("expirationDate", expirationDate);
