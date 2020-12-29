cf890f0dafdbf64042507be293b999da8cead1cf
caren1 <czarnocki.w@wp.pl>
2020-12-29 17:52:36 +0100

    modal reactmemo optimization

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,30 +1,37 @@
-import React from 'react'
+import React from "react";
 
-import Auxiliary from '../../../hoc/Auxiliary'
-import Button from '../../UI/Button/Button'
+import Auxiliary from "../../../hoc/Auxiliary";
+import Button from "../../UI/Button/Button";
 
 const OrderSummary = (props) => {
+  const ingredientSummary = Object.keys(props.ingredients).map(
+    (ingredientKey) => {
+      return (
+        <li key={ingredientKey}>
+          <span style={{ textTransform: "capitalize" }}>{ingredientKey}</span> :{" "}
+          {props.ingredients[ingredientKey]}
+        </li>
+      );
+    }
+  );
 
-    const ingredientSummary = Object.keys(props.ingredients)
-    .map(ingredientKey => {
-        return (<li key={ingredientKey}><span style={{ textTransform: "capitalize" }}>{ingredientKey}</span> : {props.ingredients[ingredientKey]}</li>
-    )});
+  return (
+    <Auxiliary>
+      <h3>Your Order</h3>
+      <p>A delicious burger with the ingredients:</p>
+      <ul>{ingredientSummary}</ul>
+      <p>
+        Total price: <em>{props.price.toFixed(2)}</em>$
+      </p>
+      <p>Continue to Checkout?</p>
+      <Button btnType="Danger" clicked={props.purchaseCanceled}>
+        CANCEL
+      </Button>
+      <Button btnType="Success" clicked={props.purchaseContinued}>
+        CONTINUE
+      </Button>
+    </Auxiliary>
+  );
+};
 
-
-    
-    return (
-        <Auxiliary>
-            <h3>Your Order</h3>
-            <p>A delicious burger with the ingredients:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p>Total price: <em>{props.price.toFixed(2)}</em>$</p>
-            <p>Continue to Checkout?</p>
-            <Button btnType="Danger" clicked={props.purchaseCanceled}>CANCEL</Button>
-            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
-        </Auxiliary>
-    )
-}
-
-export default OrderSummary
+export default OrderSummary;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -19,4 +19,9 @@ const modal = (props) => (
   </Auxiliary>
 );
 
-export default modal;
+export default React.memo(
+  modal,
+  (prevProps, nextProps) =>
+    nextProps.show === prevProps.show &&
+    nextProps.children === prevProps.children
+);
